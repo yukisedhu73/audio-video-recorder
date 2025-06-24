771adf8244fa69ef2d2ca6692e376d27518cb60f
yukisedhu <yukisedhu.r@heptagon.in>
2025-06-24 10:57:56 +0530

    chore: routes,font and tailwind setup

---
+++ src/pages/AudioVideoRecorder.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AudioVideoRecorder: React.FC = () => {
+    return (
+        <>
+        <p className='bg-amber-200 text-2xl font-bold'>
+            Title test for  Tailwind css</p></>
+    )
+}
+export default AudioVideoRecorder;
